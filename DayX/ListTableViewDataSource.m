98c2d3e530b37d3dd09585826f12757d78e16b61
Justin Huntington <corusco@gmail.com>
2015-05-04 21:07:52 -0600

    Adds code through class implementation. Compiles.

---
+++ DayX/ListTableViewDataSource.m
@@ -0,0 +1,13 @@
+//
+//  ListTableViewDataSource.m
+//  DayX
+//
+//  Created by Justin Huntington on 5/4/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import "ListTableViewDataSource.h"
+
+@implementation ListTableViewDataSource
+
+@end
